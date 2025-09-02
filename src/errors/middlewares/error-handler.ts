67e5e38488a52145b44df3fe2d5f5a314840eeed
rodhis <rodrigonoliva@gmail.com>
2025-09-02 19:04:03 -0300

    error handling middlewares and classes

---
+++ src/errors/middlewares/error-handler.ts
@@ -0,0 +1,12 @@
+import type { ErrorRequestHandler } from 'express'
+import { HttpError } from '../HttpError.js'
+
+export const errorHandlerMiddleware: ErrorRequestHandler = (error, req, res, next) => {
+    if (error instanceof HttpError) {
+        res.status(error.status).json({ message: error.message })
+    } else if (error instanceof Error) {
+        res.status(500).json({ message: error.message })
+    } else {
+        res.status(500).json({ message: 'Internal Server Error' })
+    }
+}
--- src/server.ts
+++ src/server.ts
@@ -1,14 +1,16 @@
-import cors from 'cors';
-import express from 'express';
-import {router} from './router.js';
+import cors from 'cors'
+import express from 'express'
+import { router } from './router.js'
+import { errorHandlerMiddleware } from './errors/middlewares/error-handler.js'
 
-const app = express();
+const app = express()
 
-app.use(cors());
-app.use("/api", router);
+app.use(cors())
+app.use('/api', router)
+app.use(errorHandlerMiddleware)
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3000
 
 app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+    console.log(`Server is running on http://localhost:${PORT}`)
+})
