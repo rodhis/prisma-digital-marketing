a9fbc355315b10f1adbbd5b19c3c7156c6162518
rodhis <rodrigonoliva@gmail.com>
2025-09-04 18:59:51 -0300

    get all leads route

--- src/router.ts
+++ src/router.ts
@@ -1,7 +1,15 @@
 import { Router } from 'express'
+import { LeadsController } from './controllers/LeadsController.js'
 
 const router = Router()
 
+// leads routes
+const leadsController = new LeadsController()
+
+router.get('/leads', leadsController.index)
+
+
+// status route
 router.get('/status', (req, res) => {
     res.json({ message: 'OK!' })
 })
