fcaadd35248a43329a7d6ce87d1ee144c256cce7
rodhis <rodrigonoliva@gmail.com>
2025-09-06 18:37:45 -0300

    updated status error messages

--- src/controllers/schemas/LeadsRequestSchema.ts
+++ src/controllers/schemas/LeadsRequestSchema.ts
@@ -1,12 +1,25 @@
 import z from 'zod'
 
-const CreateLeadRequestSchema = z.object({
+export const CreateLeadRequestSchema = z.object({
     name: z.string().min(1, 'Name is required'),
-    email: z.string().min(1,'Email is required'),
+    email: z.string().min(1, 'Email is required'),
     phone: z.string().min(7, 'Phone number must be at least 7 digits').optional(),
     status: z
-        .enum(['NEW', 'CONTACTED', 'QUALIFIED', 'CONVERTED', 'UNRESPONSIVE', 'DISQUALIFIED', 'ARCHIVED'])
+        .enum(['NEW', 'CONTACTED', 'QUALIFIED', 'CONVERTED', 'UNRESPONSIVE', 'DISQUALIFIED', 'ARCHIVED'], {
+            message:
+                "Status must be one of the predefined values: 'NEW', 'CONTACTED', 'QUALIFIED', 'CONVERTED', 'UNRESPONSIVE', 'DISQUALIFIED', 'ARCHIVED'",
+        })
         .optional(),
 })
 
-export { CreateLeadRequestSchema }
+export const UpdateLeadRequestSchema = z.object({
+    name: z.string().min(1, 'Requires at least 1 character to update.').optional(),
+    email: z.string().min(1, 'Requires at least 1 character to update.').optional(),
+    phone: z.string().min(7, 'Requires at least 7 characters to update.').optional(),
+    status: z
+        .enum(['NEW', 'CONTACTED', 'QUALIFIED', 'CONVERTED', 'UNRESPONSIVE', 'DISQUALIFIED', 'ARCHIVED'], {
+            message:
+                "Status must be one of the predefined values: 'NEW', 'CONTACTED', 'QUALIFIED', 'CONVERTED', 'UNRESPONSIVE', 'DISQUALIFIED', 'ARCHIVED'",
+        })
+        .optional(),
+})
