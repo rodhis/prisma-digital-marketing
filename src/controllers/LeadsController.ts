043d82a71638bba3db4c6e3ebfd9bd6d8ba7c00a
rodhis <rodrigonoliva@gmail.com>
2025-09-06 17:47:22 -0300

    created and fixed leads controller and schemas

--- src/controllers/LeadsController.ts
+++ src/controllers/LeadsController.ts
@@ -1,13 +1,32 @@
-import type { Handler } from "express";
-import prisma from "../database/index.js";
+import type { Handler } from 'express'
+import prisma from '../database/index.js'
+import { CreateLeadRequestSchema } from './schemas/LeadsRequestSchema.js'
 
 export class LeadsController {
     index: Handler = async (req, res, next) => {
         try {
-            const leads = await prisma.lead.findMany();
-            res.json(leads);
+            const leads = await prisma.lead.findMany()
+            res.json(leads)
         } catch (error) {
-            next(error);
+            next(error)
         }
-    };
+    }
+
+    create: Handler = async (req, res, next) => {
+        try {
+            const body = CreateLeadRequestSchema.parse(req.body)
+            const leadData = {
+                name: body.name,
+                email: body.email,
+                phone: body.phone ?? null,
+                ...(body.status !== undefined && { status: body.status }),
+            }
+            const newLead = await prisma.lead.create({
+                data: leadData,
+            })
+            res.status(201).json(newLead)
+        } catch (error) {
+            next(error)
+        }
+    }
 }
--- src/controllers/schemas/LeadsRequestSchema.ts
+++ src/controllers/schemas/LeadsRequestSchema.ts
@@ -2,12 +2,11 @@ import z from 'zod'
 
 const CreateLeadRequestSchema = z.object({
     name: z.string().min(1, 'Name is required'),
-    email: z.string().nonoptional('Invalid email address'),
+    email: z.string().min(1,'Email is required'),
     phone: z.string().min(7, 'Phone number must be at least 7 digits').optional(),
     status: z
         .enum(['NEW', 'CONTACTED', 'QUALIFIED', 'CONVERTED', 'UNRESPONSIVE', 'DISQUALIFIED', 'ARCHIVED'])
         .optional(),
-    campaigns: z.array(z.number().int().positive()).optional(),
 })
 
 export { CreateLeadRequestSchema }
