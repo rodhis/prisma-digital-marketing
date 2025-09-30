1d09cacab406f4491be8c31c14ed2e5a13d6ed16
rodhis <rodrigonoliva@gmail.com>
2025-09-30 18:35:29 -0300

    add lead and delete lead from group routes

--- src/controllers/GroupLeadsController.ts
+++ src/controllers/GroupLeadsController.ts
@@ -1,13 +1,18 @@
-import type { Handler } from "express";
-import type { Prisma } from "@prisma/client";
+import type { Handler } from 'express'
+import type { Prisma } from '@prisma/client'
 
-import { GetLeadsRequestSchema } from "./schemas/LeadsRequestSchemas.js";
-import prisma from "../database/index.js";
+import { GetLeadsRequestSchema } from './schemas/LeadsRequestSchemas.js'
+import { AddLeadtoGroupRequestSchema } from './schemas/GroupsRequestsSchemas.js'
+import prisma from '../database/index.js'
+import { HttpError } from '../errors/HttpError.js'
 
 export class GroupLeadsController {
     getLeads: Handler = async (req, res, next) => {
         try {
-    const groupId = Number(req.params.groupId)
+            const groupId = Number(req.params.groupId)
+            if (!groupId) {
+                throw new HttpError(400, 'Group does not exist.')
+            }
             const query = GetLeadsRequestSchema.parse(req.query)
             const { page = '1', pageSize = '10', name, status, sortBy = 'name', order = 'asc' } = query
 
@@ -31,14 +36,14 @@ export class GroupLeadsController {
                 skip: (pageNumber - 1) * pageSizeNumber,
                 take: pageSizeNumber,
                 include: {
-                    groups: true
+                    groups: true,
                 },
             })
 
             const totalLeads = await prisma.lead.count({ where })
 
             res.json({
-                data: leads,
+                leads,
                 meta: {
                     page: pageNumber,
                     pageSize: pageSizeNumber,
@@ -47,14 +52,76 @@ export class GroupLeadsController {
                 },
             })
         } catch (error) {
-            next(error);
+            next(error)
         }
     }
 
     addLead: Handler = async (req, res, next) => {
+        try {
+            const groupId = Number(req.params.groupId)
+            const body = AddLeadtoGroupRequestSchema.parse(req.body)
 
+            const existingGroup = await prisma.group.findUnique({
+                where: { id: groupId },
+            })
+            if (!existingGroup) {
+                throw new HttpError(400, 'Group does not exist.')
+            }
+
+            const existingLead = await prisma.lead.findUnique({
+                where: { id: body.leadId },
+            })
+            if (!existingLead) {
+                throw new HttpError(400, 'Lead does not exist.')
+            }
+
+            const updatedGroup = await prisma.group.update({
+                where: { id: groupId },
+                data: {
+                    leads: {
+                        connect: { id: body.leadId },
+                    },
+                },
+                include: { leads: true },
+            })
+
+            res.status(201).json(updatedGroup)
+        } catch (error) {
+            next(error)
+        }
     }
     removeLead: Handler = async (req, res, next) => {
+        try {
+            const groupId = Number(req.params.groupId)
+            const leadId = Number(req.params.leadId)
 
+            const existingGroup = await prisma.group.findUnique({
+                where: { id: groupId },
+            })
+            if (!existingGroup) {
+                throw new HttpError(400, 'Group does not exist.')
+            }
+
+            const existingLead = await prisma.lead.findUnique({
+                where: { id: leadId },
+            })
+            if (!existingLead) {
+                throw new HttpError(400, 'Lead does not exist.')
+            }
+
+            const updatedGroup = await prisma.group.update({
+                where: { id: groupId },
+                data: {
+                    leads: {
+                        disconnect: { id: leadId },
+                    },
+                },
+                include: { leads: true },
+            })
+
+            res.status(200).json(updatedGroup)
+        } catch (error) {
+            next(error)
+        }
     }
 }
