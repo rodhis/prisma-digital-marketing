2e8a352e04975fba71e584ea9addcabe59472696
rodhis <rodrigonoliva@gmail.com>
2025-09-30 18:24:30 -0300

    schema for adding a lead to a group

--- src/controllers/schemas/GroupsRequestsSchemas.ts
+++ src/controllers/schemas/GroupsRequestsSchemas.ts
@@ -9,3 +9,7 @@ export const UpdateGroupRequestSchema = z.object({
     name: z.string().min(1, "Name is required").optional(),
     description: z.string().min(4, "Description must be at least 4 characters").optional()
 });
+
+export const AddLeadtoGroupRequestSchema = z.object({
+    leadId: z.number()
+});
