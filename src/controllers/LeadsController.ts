fe59c51b34af80794d8c10c640db888c72333e6f
rodhis <rodrigonoliva@gmail.com>
2025-10-01 18:23:42 -0300

    added business rules on leads controller

--- src/controllers/LeadsController.ts
+++ src/controllers/LeadsController.ts
@@ -84,6 +84,20 @@ export class LeadsController {
             if (!existingLead) {
                 throw new HttpError(404, 'Lead not found')
             }
+
+            if (existingLead.status === 'NEW' && body.status !== undefined && body.status !== 'CONTACTED') {
+                throw new HttpError(400, "A new lead must be contacted before changing its status to other value")
+            }
+
+            if (body.status  && body.status === 'ARCHIVED') {
+                const now = new Date()
+                const diffTime = Math.abs(now.getTime() - existingLead.updatedAt.getTime())
+                const diffDays = Math.ceil(diffTime / (1000 * 60 * 60 * 24)) //converted to  seconds, then minutes, then hours and finally days
+                if (diffDays < 180) {
+                    throw new HttpError(400, "A lead can only be archived after 6 months of its last update")
+                }
+            }   
+
             const leadData = {
                 ...(body.name !== undefined && { name: body.name }),
                 ...(body.email !== undefined && { email: body.email }),
