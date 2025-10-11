1b9364ff89686b0d215055c1b7c1fc0dfe37aa9a
rodhis <rodrigonoliva@gmail.com>
2025-10-11 18:10:15 -0300

    leads repository methods

---
+++ src/repositories/LeadsRepository.ts
@@ -0,0 +1,43 @@
+import type { Lead } from '@prisma/client'
+
+export type LeadStatus =
+    | 'NEW'
+    | 'CONTACTED'
+    | 'QUALIFIED'
+    | 'CONVERTED'
+    | 'UNRESPONSIVE'
+    | 'DISQUALIFIED'
+    | 'ARCHIVED'
+
+export interface LeadWhereParams {
+    name?: {
+        like?: string
+        equals?: string
+        mode?: 'default' | 'insensitive'
+    }
+    status?: LeadStatus
+}
+
+export interface FindLeadsParams {
+    where?: LeadWhereParams
+    sortBy?: 'name' | 'status' | 'createdAt'
+    order?: 'asc' | 'desc'
+    limit?: number
+    offset?: number
+}
+
+export interface CreateLeadAttributes {
+    name: string
+    email: string
+    phone?: string
+    status?: LeadStatus
+}
+
+export interface LeadsRepository {
+    find: (params: FindLeadsParams) => Promise<Lead[]>
+    findById: (id: number) => Promise<Lead | null>
+    count: (where: LeadWhereParams) => Promise<number>
+    create: (attributes: CreateLeadAttributes) => Promise<Lead>
+    updateById: (id: number, attributes: Partial<CreateLeadAttributes>) => Promise<Lead | null>
+    deleteById: (id: number) => Promise<Lead | null>
+}
